4f3359637fd3451653e7f40e9fe008f53993d4d3
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-05-15 23:55:01 +0700

    added a Like Button on the Details Page

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,11 +1,12 @@
 import TheMovieDbSource from '../../data/themoviedb-source';
 import UrlParser from '../../routes/url-parser';
-import { createMovieDetailTemplate } from '../templates/template-creator';
+import { createLikeButtonTemplate, createMovieDetailTemplate } from '../templates/template-creator';
 
 const Detail = {
   async render() {
     return `
       <div id="movie" class="movie"></div>
+      <div id="likeButtonContainer"></div>
     `;
   },
 
@@ -14,6 +15,9 @@ const Detail = {
     const movie = await TheMovieDbSource.detailMovie(url.id);
     const movieContainer = document.querySelector('#movie');
     movieContainer.innerHTML = createMovieDetailTemplate(movie);
+
+    const likeButtonContainer = document.querySelector('#likeButtonContainer');
+    likeButtonContainer.innerHTML = createLikeButtonTemplate();
   },
 
 };
